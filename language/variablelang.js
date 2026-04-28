d83f5b418c6181792ffddf00cbe256ab2a979859
Anna Can <annakaayn@gmail.com>
2026-04-28 14:24:28 -0400

    Create variablelang.js

---
+++ language/variablelang.js
@@ -0,0 +1,12 @@
+// Source - https://stackoverflow.com/a/46008865
+// Posted by M. Waheed, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-04-28, License - CC BY-SA 3.0
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script>
+
+    $(document).ready(function(){
+    $('#div1').text(language.date);
+    });
+
+    </script>
